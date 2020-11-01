57e5b24226ca62b8b688df0d62ccaf557aeaa3dc
Kevin Urbainczyk <kevin@rays3t.info>
2020-11-01 13:58:07 +0100

    Move HA data classes to subdirectory

---
+++ MqttPcHeartbeatMonitor/homeassistant/HomeAssistantLockConfig.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace MqttPcHeartbeatMonitor
+{
+    public class HomeAssistantLockConfig
+    {
+        [JsonProperty("name")] public string Name;
+        [JsonProperty("device_class")] public string DeviceClass { get; set; }
+        [JsonProperty("state_topic")] public string StateTopic { get; set; }
+
+        [JsonProperty("device")] public HomeAssistantDevice Device { get; set; }
+
+        [JsonProperty("unique_id")] public string UniqueId { get; set; }
+    }
+}
